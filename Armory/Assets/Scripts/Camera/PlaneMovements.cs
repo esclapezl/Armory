61f9c3993d8fb82e78e0f45be389c1f143ea42b5
Esclapez Loïc <loicesclapez@gmail.com>
2024-03-13 00:25:48 +0100

    random sprites test

--- Armory/Assets/Scripts/Camera/PlaneMovements.cs
+++ Armory/Assets/Scripts/Camera/PlaneMovements.cs
@@ -7,10 +7,12 @@ namespace Camera
     public class PlaneMovements : MonoBehaviour
     {
         [NonSerialized] private Transform _cameraTransform;
+        [NonSerialized] private Vector3 _originalPosition;
 
         private void Awake()
         {
             _cameraTransform = ObjectSearch.FindRoot("Main Camera");
+            _originalPosition = transform.position;
         }
 
         private void FixedUpdate()
@@ -23,15 +25,15 @@ namespace Camera
             float depth = 0;
             if (transform.position.z < 0)
             {
-                depth = Mathf.Abs(transform.position.z) / _cameraTransform.position.z;
+                depth = Mathf.Abs(transform.position.z) / _cameraTransform.localPosition.z;
             }
             else if (transform.position.z > 0)
             {
-                depth = 1 + transform.position.z / _cameraTransform.position.z;
+                depth = 1 + transform.position.z / _cameraTransform.localPosition.z;
             }
 
-            float posX = _cameraTransform.position.x * depth;
-            float posY = _cameraTransform.position.y * depth;
+            float posX = _originalPosition.x + _cameraTransform.localPosition.x * depth;
+            float posY = _originalPosition.y + _cameraTransform.localPosition.y * depth;
 
             transform.position = new Vector3(posX, posY, transform.position.z);
         }
