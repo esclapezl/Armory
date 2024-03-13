fb81417bcf122c6f96b513c66ddaa538b7e9e464
Esclapez Loïc <loicesclapez@gmail.com>
2024-03-13 16:26:22 +0100

    scene change

--- Armory/Assets/Scripts/Levels/LevelSelection/LevelSelection.cs
+++ Armory/Assets/Scripts/Levels/LevelSelection/LevelSelection.cs
@@ -1,79 +1,80 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Utils;
 
-public class LevelSelection : MonoBehaviour
+namespace Levels.LevelSelection
 {
-    [NonSerialized] private List<Transform> _levelTransforms;
-    [NonSerialized] private int _selectedLevel = 0;
-    [NonSerialized] private GameManager _gameManager;
-    [SerializeField] private GameObject levelSelectorPrefab;
-
-    private void Awake()
+    public class LevelSelection : MonoBehaviour
     {
-        _gameManager = ObjectSearch.FindRoot("GameManager").GetComponent<GameManager>();
-        _levelTransforms = new List<Transform>();
-    }
+        [NonSerialized] private List<Transform> _levelTransforms;
+        [NonSerialized] private int _selectedLevel = 0;
+        [NonSerialized] private GameManager _gameManager;
+        [SerializeField] private GameObject levelSelectorPrefab;
 
-    private void Start()
-    {
-        for (int i = 1; i < _gameManager.LevelFolder.childCount; i++)
+        private void Awake()
         {
-            GameObject level = Instantiate(levelSelectorPrefab, _gameManager.LevelFolder.GetChild(i), true);
-            level.transform.localPosition = new Vector3(10*i, 0, 0);
-            level.name = (i) + "_" + _gameManager.LevelFolder.GetChild(i).name;
-            _levelTransforms.Add(level.transform);
-            ClearHover(i-1);
+            _gameManager = ObjectSearch.FindRoot("GameManager").GetComponent<GameManager>();
+            _levelTransforms = new List<Transform>();
         }
-    }
 
-    private void Update()
-    {
-        if (_gameManager.currentLevelNumber == 0)
+        private void Start()
         {
-            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            for (int i = 1; i < _gameManager.LevelFolder.childCount; i++)
             {
-                if(_selectedLevel > 0)
-                {
-                    ClearHover(_selectedLevel);
-                    _selectedLevel--;
-                    HoverLevel(_selectedLevel);
-                }
+                GameObject level = Instantiate(levelSelectorPrefab, _gameManager.LevelFolder.GetChild(i), true);
+                level.transform.localPosition = new Vector3(10*i, 0, 0);
+                level.name = (i) + "_" + _gameManager.LevelFolder.GetChild(i).name;
+                _levelTransforms.Add(level.transform);
+                ClearHover(i-1);
             }
-            else if(Input.GetKeyDown(KeyCode.RightArrow))
+        }
+
+        private void Update()
+        {
+            if (_gameManager.currentLevelNumber == 0)
             {
-                if(_selectedLevel < _levelTransforms.Count - 1)
+                if(Input.GetKeyDown(KeyCode.LeftArrow))
                 {
-                    ClearHover(_selectedLevel);
-                    _selectedLevel++;
-                    HoverLevel(_selectedLevel);
+                    if(_selectedLevel > 0)
+                    {
+                        ClearHover(_selectedLevel);
+                        _selectedLevel--;
+                        HoverLevel(_selectedLevel);
+                    }
                 }
-            }
+                else if(Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    if(_selectedLevel < _levelTransforms.Count - 1)
+                    {
+                        ClearHover(_selectedLevel);
+                        _selectedLevel++;
+                        HoverLevel(_selectedLevel);
+                    }
+                }
         
-            if(Input.GetKeyDown(KeyCode.Return))
-            {
-                StartLevel(_selectedLevel);
+                if(Input.GetKeyDown(KeyCode.Return))
+                {
+                    StartLevel(_selectedLevel);
+                }
             }
         }
-    }
 
-    private void HoverLevel(int levelIndex)
-    {
-        SpriteRenderer spriteRenderer = _levelTransforms[levelIndex].GetComponent<SpriteRenderer>();
-        spriteRenderer.color = new Color(1f, 0, 0, 1f);
-    }
+        private void HoverLevel(int levelIndex)
+        {
+            SpriteRenderer spriteRenderer = _levelTransforms[levelIndex].GetComponent<SpriteRenderer>();
+            spriteRenderer.color = new Color(1f, 0, 0, 1f);
+        }
     
-    private void ClearHover(int levelIndex)
-    {
-        SpriteRenderer spriteRenderer = _levelTransforms[levelIndex].GetComponent<SpriteRenderer>();
-        spriteRenderer.color = new Color(0, 0, 0, 0.5f);
-    }
+        private void ClearHover(int levelIndex)
+        {
+            SpriteRenderer spriteRenderer = _levelTransforms[levelIndex].GetComponent<SpriteRenderer>();
+            spriteRenderer.color = new Color(0, 0, 0, 0.5f);
+        }
 
-    private void StartLevel(int levelIndex)
-    {
-        _gameManager.StartLevel(_selectedLevel);
+        private void StartLevel(int levelIndex)
+        {
+            _gameManager.StartLevel(_selectedLevel);
+        }
     }
 }
