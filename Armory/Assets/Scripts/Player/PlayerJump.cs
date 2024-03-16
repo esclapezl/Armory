eb7524f1075aba6b15d4db681080885f456f4679
Esclapez Loïc <loicesclapez@gmail.com>
2024-03-16 16:25:22 +0100

    adaptative pjump

--- Armory/Assets/Scripts/Player/PlayerJump.cs
+++ Armory/Assets/Scripts/Player/PlayerJump.cs
@@ -12,7 +12,7 @@ namespace Player
         [NonSerialized] private PlayerMovements _playerMovements;
         [NonSerialized] private PlayerKnockback _playerKnockback;
         [NonSerialized] private Player _player;
-        
+
         [SerializeField] public LayerMask whatIsGround;
         [NonSerialized] private Transform _groundCheck;
         [NonSerialized] public bool Grounded;
@@ -28,25 +28,39 @@ namespace Player
         [NonSerialized] private bool _jumpInput;
         [NonSerialized] private Rigidbody2D _rigidbody2D;
 
+        [Range(0, 1f)] [SerializeField] private float jumpHoldTime = 0.2f;
+        [SerializeField] private float jumpHoldForce = 2f;
+
+        private float jumpHold;
+        private bool _holdingJump;
+
         private void Awake()
         {
             _playerCrouch = GetComponent<PlayerCrouch>();
             _playerMovements = GetComponent<PlayerMovements>();
             _playerKnockback = GetComponent<PlayerKnockback>();
             _player = GetComponent<Player>();
-            
+
             _groundCheck = ObjectSearch.FindChild(transform, "GroundCheck");
             _rigidbody2D = GetComponent<Rigidbody2D>();
         }
-        
+
         private void Update()
         {
-            if (!_player.dead && Input.GetButtonDown("Jump"))
+            if (!_player.dead)
             {
-                _jumpInput = true;
+                _holdingJump = Input.GetButton("Jump");
+                if (Input.GetButtonDown("Jump"))
+                {
+                    _jumpInput = true;
+                }
+                else if (Input.GetButtonUp("Jump"))
+                {
+                    jumpHold = 0;
+                }
             }
         }
-        
+
         private void FixedUpdate()
         {
             if (!_player.dead)
@@ -77,6 +91,7 @@ namespace Player
             {
                 if (Grounded || _coyoteTime > 0) //check for ground or remaining coyote time
                 {
+                    jumpHold = jumpHoldTime;
                     Jump();
                 }
                 else
@@ -89,6 +104,11 @@ namespace Player
                 _coyoteTime = -1f;
                 if (_jumpBuffer > 0) //check for remaining jump buffer
                 {
+                    if (_holdingJump)
+                    {
+                        jumpHold = jumpHoldTime;
+                    }
+
                     Jump();
                 }
             }
@@ -107,6 +127,13 @@ namespace Player
                 {
                     _jumpBuffer = Mathf.Max(_jumpBuffer - Time.fixedDeltaTime, 0);
                 }
+            }
+
+            if (jumpHold > 0)
+            {
+                float appliedJumpForce = jumpHoldForce * jumpHold / jumpHoldTime;
+                _rigidbody2D.AddForce(new Vector2(0f, appliedJumpForce));
+                jumpHold = Mathf.Max(jumpHold - Time.fixedDeltaTime, 0);
             }
         }
 
