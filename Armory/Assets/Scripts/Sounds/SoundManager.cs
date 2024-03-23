01359c10d27f267839f795d6cfde39f5c52034a9
Esclapez Loïc <loicesclapez@gmail.com>
2024-03-24 00:02:33 +0100

    scene

--- Armory/Assets/Scripts/Sounds/SoundManager.cs
+++ Armory/Assets/Scripts/Sounds/SoundManager.cs
@@ -1,19 +1,24 @@
+using System;
 using UnityEngine;
 
 namespace Sounds
 {
     public class SoundManager : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] public AudioClip[] soundClips;
+        [NonSerialized] private AudioSource _audioSource;
+
         void Start()
         {
-        
+            _audioSource = gameObject.AddComponent<AudioSource>();
         }
 
-        // Update is called once per frame
-        void Update()
+        public void PlaySound(int clipIndex, float volume = 1f, float pitch = 1f)
         {
-        
+            _audioSource.clip = soundClips[clipIndex];
+            _audioSource.volume = volume;
+            _audioSource.pitch = pitch;
+            _audioSource.Play();
         }
     }
 }
